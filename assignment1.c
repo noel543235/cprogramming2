c86de6e7341a379fe13adba9d7685a8136849588
Adriana <aviviano@me.com>
2025-01-17 13:48:57 -0500

    sencond

--- assignment1.c
+++ assignment1.c
@@ -9,7 +9,13 @@ while(num > 0){
 }
 
 if(num == 0){
-    printf("Happy New Year!");
+    printf("Happy New Year!\n");
+}
+
+int num2 = 10;
+while(num2 > 0){
+    printf("%d \n", num2);
+    num2-=2;
 }
 return 1;
 }
