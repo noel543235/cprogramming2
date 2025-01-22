ef8e9937a5400656af2defe460f6b08ef26963bf
Adriana <aviviano@me.com>
2025-01-22 13:39:32 -0500

    finals

--- assignment4.c
+++ assignment4.c
@@ -11,18 +11,24 @@ int* ptr = &num;
 printf("%p\n",ptr);
 
 printf("%d\n",*ptr);
+int  temp = sizeof(*ptr);
+printf("%d\n", temp);
 
 char* ptr2 = &letter;
 
 printf("%p\n",ptr2);
 
 printf("%c\n",*ptr2);
+char  temp2 = sizeof(*ptr2);
+printf("%c\n", temp2);
 
 float* ptr3 = &flo;
 
 printf("%p\n",ptr3);
 
 printf("%f\n",*ptr3);
+float  temp3 = sizeof(*ptr3);
+printf("%f\n", temp3);
 
 int num1 = 50;
 int num2 = 60;
